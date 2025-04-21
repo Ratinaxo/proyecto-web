9db08bb9eb67981866b512105c07bae9aeb55c8c
Ratinaxo <ricardo.toro.m@mail.pucv.cl>
2025-04-20 21:53:24 -0400

    creado login primera pagina

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [AppComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+  exports: [HeaderComponent, FooterComponent]
+})
+export class AppModule {}
